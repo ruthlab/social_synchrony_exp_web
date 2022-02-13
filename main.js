37a035845058319925016c1087bfcfbcbdea8335
chenr <chenraccah@gmail.com>
2022-02-13 21:04:29 +0200

    add arousal

--- main.js
+++ main.js
@@ -396,6 +396,62 @@ function dataTagTrail(){
     } , 100);
 }
 
+function playVidVal() {
+    var vid = document.getElementById('thevideov');
+    vid.play();
+    var btndiv = document.getElementById('btndivv');
+    var btn = document.getElementById('playbtnv');
+    btndiv.removeChild(btn);
+    dataTagVal();
+}
+
+function dataTagVal(){
+    let con = true;
+    var startTime = Date.now();
+    document.getElementById('thevideov').addEventListener('ended',myHandler,false);
+    function myHandler(e) {
+        con = false;
+    }
+    var interval = window.setInterval(function () {
+        if (con){
+            var sliderValX = slider.position.x.toString();
+            var sliderValY = slider.position.y.toString();
+            var elapsedTime = Date.now() - startTime;
+            appendToInnerDict('valence', {"time":(elapsedTime / 1000).toFixed(1), "x": sliderValX,"y": sliderValY});
+            // console.log((elapsedTime / 1000).toFixed(1) ,sliderVal);
+        }
+        else {
+            window.clearInterval(interval);
+            var body = document.getElementById('bodyv')
+            var videodiv = document.getElementById('videodivv')
+            var graph = document.getElementById('widget')
+            body.removeChild(videodiv);
+            body.removeChild(graph);
+            var diver = document.createElement("div");
+            diver.classList.add("text-xl-center");
+            var tutorial = document.createElement("img");
+            tutorial.classList.add("w-50");
+            tutorial.src="next_movie.png";
+            var btn = document.createElement('button');
+            var link = document.createElement('a');
+            var next = document.createTextNode("המשך");
+            btn.classList.add("btn");
+            btn.classList.add("btn-primary");
+            btn.classList.add("my-1");
+            link.classList.add("text-decoration-none");
+            link.classList.add("text-white");
+            link.appendChild(next);
+            link.href = "rangeTagValence.html";
+            btn.appendChild(link);
+            var br = document.createElement('br');
+            onDownload()
+            diver.appendChild(tutorial);
+            diver.appendChild(br);
+            diver.appendChild(btn);
+            body.appendChild(diver);
+        }
+    } , 100);
+}
 /**
  example: uploadFileExample("name", "application/json", "1PWmDDeCRfEgn602o3mQjEDIJnDq8gZDn", "dataaa", access_token)
  */
